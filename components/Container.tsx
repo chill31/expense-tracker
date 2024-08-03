876c10631ebbd5c1b341ab4ff751fd6be67afba5
chill31 <89910163+chill31@users.noreply.github.com>
2024-08-03 21:03:34 +0530

    fixed calling local storage without hook

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,25 +1,34 @@
-'use client'
+"use client";
 
 import { usePathname } from "next/navigation";
 import Sidebar from "./Sidebar";
+import { useEffect } from "react";
 
-export default function Container({ children, className }: { children: React.ReactNode, className?: string }) {
+export default function Container({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  useEffect(() => {
+    const transactions = localStorage.getItem("transactions");
+    const budgets = localStorage.getItem("budgets");
 
-  const transactions = localStorage.getItem("transactions");
-  const budgets = localStorage.getItem("budgets");
-
-  if (!transactions) {
-    localStorage.setItem("transactions", JSON.stringify([]));
-  }
-  if(!budgets) {
-    localStorage.setItem("budgets", JSON.stringify([]));
-  }
+    if (!transactions) {
+      localStorage.setItem("transactions", JSON.stringify([]));
+    }
+    if (!budgets) {
+      localStorage.setItem("budgets", JSON.stringify([]));
+    }
+  });
 
   const path = usePathname();
-  console.log(path);
 
   return (
-    <div className={`w-screen min-h-screen overflow-x-hidden bg-dark ${className}`}>
+    <div
+      className={`w-screen min-h-screen overflow-x-hidden bg-dark ${className}`}
+    >
       <Sidebar currentPath={path} />
       {children}
     </div>
